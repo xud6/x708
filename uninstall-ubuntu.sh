33f3da6ac462ffcd3328fc6b16f592e37ca5ed10
geekworm <46470628+geekworm-com@users.noreply.github.com>
2021-07-21 15:54:44 +0800

    Add files via upload

---
+++ uninstall-ubuntu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#remove x-c1 old installtion
+#SYS_RUN_FILE=/etc/bash.bashrc
+#sudo sed -i '/pigpiod/d' ${SYS_RUN_FILE}
+#sudo sed -i '/x-c1/d' ${SYS_RUN_FILE}
+
+sudo rm /usr/local/bin/x-c1-softsd.sh -f
+sudo rm /etc/x-c1pwr.sh -f
